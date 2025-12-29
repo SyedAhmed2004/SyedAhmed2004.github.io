46849e268b0505c09e9542c12d5ee37e623ef9c9
Syed Ahmed <yk23oz@brocku.ca>
2025-12-28 19:24:20 -0500

    deploy static site via docs folder

---
+++ docs/_next/static/chunks/587cc942c3a5dfe7.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/3d36a0d93b4752d1.js",
+  "static/chunks/197cd7a55570e2eb.js",
+  "static/chunks/08a82110177f456b.js",
+  "static/chunks/24ee2e8ae793a94f.js",
+  "static/chunks/turbopack-b4a16e06273a9101.js"
+])
---
+++ docs/_next/static/chunks/607198f9512e829b.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/5d823d368fd3a1c2.js",
+  "static/chunks/24ee2e8ae793a94f.js",
+  "static/chunks/197cd7a55570e2eb.js",
+  "static/chunks/1cd2f109f3d9ae61.css",
+  "static/chunks/turbopack-3d9992b1700b6762.js"
+])
