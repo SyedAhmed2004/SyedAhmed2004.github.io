4b66db8a711662dec7871d7804ed50b060a9b8af
Syed Ahmed <yk23oz@brocku.ca>
2025-12-30 20:54:44 -0500

    deploy static site

---
+++ docs/_next/static/60DCTi8HGXwKVSRiwwZ_y/_buildManifest.js
@@ -0,0 +1,26 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/6e606eb84aeb6132.js"
+  ],
+  "/_error": [
+    "static/chunks/2fccf53614a50a16.js"
+  ],
+  "/projects/ga-image-compression": [
+    "static/chunks/88d6d41b35403d99.js"
+  ],
+  "/projects/web-development": [
+    "static/chunks/e16a553b81def933.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/projects/ga-image-compression",
+    "/projects/web-development"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ docs/_next/static/chunks/da3654b3e33d5fb8.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e988f70b61ca5551.js",
+  "static/chunks/8f7a10dbedaa19eb.js",
+  "static/chunks/1b7ff8eaea25f3a7.css",
+  "static/chunks/turbopack-cf131b13f734a70f.js"
+])
